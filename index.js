db8f54565550268f24ebf4c48faa4808b75bfb11
Kiran Sardar Ahmad <93036283+Kiransardarahmad19@users.noreply.github.com>
2024-03-31 13:00:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 8000);
+const http = require('http');
+
+const options = {
+  hostname: 'localhost',
+  port: 8000, // Change port to 8000
+  path: '/',
+  method: 'GET'
+};
+
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`Problem with request: ${e.message}`);
+});
+
+// End the request
+req.end();
